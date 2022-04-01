ba53f9fedc6d5907f3f3227822d51ddaf578efb9
slickcodes <hartpaulisimo@gmail.com>
2022-04-01 14:02:55 +0100

    Make mobile mode navigatio dropdown animate properly , worked on the course card on the home page, and reduce the slider

--- assets/scripts/home.js
+++ assets/scripts/home.js
@@ -1,4 +1,4 @@
-const slideImageArray = ['slide1.jpg' , 'slide2.jpg' ,'slide3.jpg', 'slide4.jpg' ,'slide5.jpg' ,'slide6.jpg','slide7.jpg','slide8.jpg']
+const slideImageArray = ['slide1.jpg' , 'slide2.jpg' ,'slide3.jpg']
 
 const generateSlideNodes = function(){
     const firstSlide = null 
--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,77 +1,92 @@
-const navigation = document.querySelector('nav.nav')
-const navToggleButton = document.querySelector('.nav__menu')
-const navLogo = document.querySelector('.nav__logo')
-const navLinks = document.querySelectorAll('label.dropdown__link')
-const navSlider = document.querySelector('.nav__link-container')
-const navDesktopViewScreen = 1000
+const navigation = document.querySelector("nav.nav");
+const navToggleButton = document.querySelector(".nav__menu");
+const navLogo = document.querySelector(".nav__logo");
+const navLinks = document.querySelectorAll("label.dropdown__link");
+const navSlider = document.querySelector(".nav__link-container");
+const navDesktopViewScreen = 1000;
 
-
-console.log(navLinks)
-
-function resetMenu( node ) {
-    const dropdown = document.querySelectorAll('.dropdown')
-    dropdown.forEach(list => (list != node)&&  list.classList.remove('show') )
+function resetMenu(node) {
+	const dropdown = document.querySelectorAll(".dropdown");
+	dropdown.forEach((list) => list != node && list.classList.add("hide"));
 }
 
 navToggleButton.onclick = function (event) {
-    navigation.classList.toggle('active')
-    event.target.classList.toggle('hide')
-    resetMenu()
+	if (navigation.classList.contains("active"))
+		navigation.ontransitionend = () => {
+			document.querySelectorAll("nav .dropdown").forEach((dropdown) => {
+				dropdown.style.height = 0;
+			});
+			resetMenu();
+		};
+	else navigation.ontransitionend = null;
+
+	navigation.classList.toggle("active");
+	event.target.classList.toggle("hide");
+};
+
+const mainContainer = document.querySelector("main");
+
+
+function solidNavigationEffect(event) {
+	const mainRect = mainContainer.getBoundingClientRect().top;
+	const calc = mainRect - navigation.clientHeight;
+	const scrollUpButton = document.querySelector(".up__button");
+
+	if (calc <= 0) {
+		navigation.classList.remove("transparent");
+		scrollUpButton.classList.add("show");
+		return;
+	}
+	navigation.classList.add("transparent");
+	scrollUpButton.classList.remove("show");
 }
 
 
-function fixLogo() {
-    resetMenu()
-    if (window.innerWidth < navDesktopViewScreen) {
-        const image = window.innerWidth > navDesktopViewScreen ? 'BVC-Logo-1.png' : 'BVC-Logo-2.png';
-        // navLogo.src = `assets/images/${image}`
-    }
-}
+
+const dropdownHeightUpdater = function () {
+
+	console.log('-------------------')
+	const dropdownContainers = document.querySelectorAll(".dropdown__container");
+
+	dropdownContainers.forEach(function (dropdownCont, index) {
+		if (window.innerWidth > navDesktopViewScreen) return;
+
+		const dropdown = dropdownCont.querySelector(".dropdown");
+		dropdown.classList.remove("hide");
 
 
-navLinks.forEach((navItem, index) => {
+		const dropdownHeight = dropdown.clientHeight;
+		console.log("clientHeight", dropdownHeight);
 
-    navItem.onclick = function (event) {
-        event.preventDefault()
-    if(window.innerWidth >= navDesktopViewScreen)
-        return resetMenu()
-        
-        event.stopPropagation()
-        const dropdown = document.querySelectorAll('.dropdown')
-        
-        resetMenu(dropdown[index])
-        dropdown[index].classList.toggle('show')
+		dropdown.ontransitionend = function () {
+			console.log('srink')
+		}
+		dropdown.classList.add("hide");
 
-    }
+		dropdownCont.querySelector(".link").onclick = function (event) {
 
-})
+			if (dropdown.classList.contains("hide")) {
+				dropdown.style.height = dropdownHeight + "px";
+			} else {
+				dropdown.style.height = 0;
+			}
 
-
-
-
-const mainContainer = document.querySelector('main')
-
-function solidNavigationEffect(event){
-    const mainRect = mainContainer.getBoundingClientRect().top
-    const calc = mainRect - navigation.clientHeight
-    const scrollUpButton = document.querySelector('.up__button')
-    
-    if( calc <= 0){
-        navigation.classList.remove('transparent')
-        scrollUpButton.classList.add('show')
-        return
-    }
-        navigation.classList.add('transparent')
-        scrollUpButton.classList.remove('show')
-}
+			dropdown.classList.toggle("hide");
+		};
+	});
+};
 
 
-window.onscroll = solidNavigationEffect
+
+window.onscroll = function () {
+	solidNavigationEffect()
+};
+
 
 window.onload = function () {
-    fixLogo()
-    solidNavigationEffect()
-}
+	// resetMenu()
+	solidNavigationEffect();
+	dropdownHeightUpdater()
+	// window.onresize = dropdownHeightUpdater
 
-
-
+};
