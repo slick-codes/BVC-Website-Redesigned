c5d1c7a164b2c13e5b56bc41f3328d43bb922a7a
slickcodes <hartpaulisimo@gmail.com>
2022-04-10 12:38:50 +0100

    Fix faq animation with javascript

---
+++ assets/scripts/faq.js
@@ -0,0 +1,55 @@
+
+
+
+const faqCard = document.querySelectorAll('.faq .faq_card')
+// const hideClassName = '--hide' 
+const dropdownTransitionDuration = '.1s'
+
+const clearFaqCardClass = function( exclude ){
+    faqCard.forEach( card => {
+        const icon = card.querySelector('.faq_card__title > div:first-child i')
+        const iconContainer = card.querySelector('.faq_card__title > div:first-child')
+        if( card !== exclude ){
+            card.querySelector('.faq_card__dropdown').style.height = 0
+            iconContainer.style.transform = 'rotate(0deg)'
+            icon.classList.replace('fa-minus' , 'fa-plus')
+        }
+    })
+}
+
+
+function faqDropDown(){
+faqCard.forEach( card => {
+    const cardTitle = card.querySelector('.faq_card__title')
+    const dropdown = card.querySelector('.faq_card__dropdown')
+    const height = dropdown.clientHeight
+    dropdown.style.height = 0
+    
+    
+    setTimeout( () => dropdown.style.transitionDuration = dropdownTransitionDuration, 300 )
+    
+
+    console.log(dropdown)
+    
+    cardTitle.onclick = function(event){
+        clearFaqCardClass(card)
+        const icon = card.querySelector('.faq_card__title > div:first-child i')
+        const iconContainer = card.querySelector('.faq_card__title > div:first-child')
+
+        if( dropdown.style.height <= 0 + 'px'){
+            dropdown.style.height = height + 25 + 'px'
+            iconContainer.style.transform = 'rotate(180deg)'
+            icon.classList.replace('fa-plus' , 'fa-minus')
+        }else{
+            dropdown.style.height = 0
+            icon.classList.replace('fa-minus' , 'fa-plus')
+            iconContainer.style.transform = 'rotate(0deg)'
+        }
+    }
+})
+}
+
+window.onload = function(){ // Overiding the main.js file 
+    dropdownHeightUpdater() //  Overidig main.js file
+    faqDropDown()
+}
